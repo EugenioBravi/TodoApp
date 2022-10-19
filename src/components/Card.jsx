c67a7edb8b5c49d4ac3e55a2102885635fa127a8
EugenioBravi <bravieugenio@gmail.com>
2022-10-19 14:16:44 -0300

    feat: Add a card when form in send

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,26 +1,24 @@
-const Card = () => {
+const Card = ({ task }) => {
+  console.log(task.date);
   return (
-    <a className="group block h-56" href="/blog">
-  <div
-    className="relative flex h-full items-end rounded-3xl border-4 border-black bg-white p-8 transition group-hover:-translate-x-2 group-hover:-translate-y-2 group-hover:shadow-[8px_8px_0_0_#000]"
-  >
-    <div className="lg:group-hover:absolute lg:group-hover:opacity-0">
-      <span className="text-3xl sm:text-4xl" role="img" aria-hidden="true">📆</span>
-      <p className="mt-4 text-xl font-bold sm:text-2xl">Datepicker Clear</p>
-    </div>
+    <div className="group mb-4 block h-56">
+      <div className="relative flex h-full items-start rounded-3xl border-4 border-black bg-white p-8 transition group-hover:-translate-x-2 group-hover:-translate-y-2 group-hover:shadow-[8px_8px_0_0_#000]">
+        <div className="group-hover:absolute group-hover:opacity-0">
+          <span className="text-xl" role="img" aria-hidden="true">
+            📆 {task.date}
+          </span>
+          <p className="mt-4 text-xl font-bold sm:text-2xl uppercase">{task.info.task}</p>
+        </div>
 
-    <div
-      className="absolute opacity-0 lg:group-hover:relative lg:group-hover:opacity-100"
-    >
-      <h3 className="text-2xl font-bold">Datepicker Clear</h3>
+        <div className="absolute opacity-0 group-hover:relative group-hover:opacity-100">
+          <h3 className="text-2xl font-bold uppercase">{task.info.task}</h3>
 
-      <p className="mt-4 text-lg font-medium leading-relaxed">
-        Datepicker input using Flatpickr with a button that clears the selected
-        date.
-      </p>
+          <p className="mt-4 text-lg font-medium leading-relaxed">
+            {task.info.description}
+          </p>
+        </div>
+      </div>
     </div>
-  </div>
-</a>
-  )
-}
-export default Card
+  );
+};
+export default Card;
--- src/components/TaskContainer.jsx
+++ src/components/TaskContainer.jsx
@@ -1,10 +1,14 @@
+import { useTaskContext } from "./Context/TaskContext";
 import Card from "./Card";
 
 const TaskContainer = () => {
-
+  const {tasks} = useTaskContext()
   return <div className="h-full w-full bg-green-100 px-10">
     <h1 className="font-bold font-serif text-center my-4 text-6xl ">Tasks to do</h1>
-    <Card />
+    {
+      tasks.map(task => <Card task={task} key={task.id}/>)
+    }
+    
   </div>;
 };
 export default TaskContainer;
