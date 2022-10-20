0204f7a77686fd5569235f5f8f8f8b1a2d21a185
EugenioBravi <bravieugenio@gmail.com>
2022-10-20 14:07:30 -0300

    Style

--- src/components/SideMenu.jsx
+++ src/components/SideMenu.jsx
@@ -2,7 +2,7 @@ import Form from "./Form";
 
 const SideMenu = () => {
   return (
-    <div className="container h-screen w-[600px] bg-cyan-100 p-4 ">
+    <div className="container flex  h-screen w-full max-w-md flex-col items-center  p-4 border-r bg-slate-50 border-slate-700 ">
       <Form />
     </div>
   );
